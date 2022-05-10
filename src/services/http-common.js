2dcec3a768df8e8a0602adb52f55ceec1f340e38
Antony <mirasandyantony@gmail.com>
2022-05-10 09:56:48 +0700

    config-env-file

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
-const apiKey = "109f188e595b16ec3ba8282931fc3c60"; 
+const apiKey = process.env.VUE_APP_API_KEY_WEATHER; 
 export const APIData = axios.create({
-    baseURL:`https://api.openweathermap.org/data/2.5/weather`,
-    // headers:{
-    //     'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
-    //     'X-RapidAPI-Key': '1b5133b3d3msh3c8dab155dc997ap14c8e1jsn0db4fd42710b'
-    // },
+    baseURL: process.env.VUE_APP_BASE_URL_WEATHER,
     params:{
         q: String,
         appid: apiKey
@@ -14,9 +10,9 @@ export const APIData = axios.create({
 })
 
 
-const apiKeyCity = "8hUu5fIezDEYEBWWiqZxPA==O41WhKwZfca72ZC2"
+const apiKeyCity = process.env.VUE_APP_API_KEY_CITY
 export const APICity = axios.create({
-    baseURL:`https://api.api-ninjas.com/v1/city`,
+    baseURL: process.env.VUE_APP_BASE_URL_CITY,
     headers:{
         'X-Api-Key':apiKeyCity,
     },
