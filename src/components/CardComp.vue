70d5ccee95a40e6be36ef739bb415ad61bf391ca
Antony <mirasandyantony@gmail.com>
2024-06-18 00:05:30 +0700

    Adjustment card component

--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -1,41 +1,45 @@
 <template>
-  <div class="container mx-auto p-2 border-solid border-2 rounded-lg shadow-lg bg-white">
+  <div class="md:mx-auto w-4/5 h-max p-2 border-solid border-2 rounded-lg shadow-lg bg-white">
     <!-- <h1>{{dataWeather}}</h1> -->
     <p class="font-sand font-bold text-xl text-left">
       Current Weather
     </p>
-    <p class="font-sand font-bold text-3xl text-center">
+    <p class="font-sand font-bold text-xl text-center">
       {{ dataWeather.name }}
     </p>
 
-    <div class="flex justify-around w-full h-40">
-      <div class="basis-1/4">
-        <div class="flex justify-around">
-          <img class="xl:w-32 sm:w-12" :src="require(`../assets/images/icons/thermometer.svg`)" alt="themometer">
-          <p class="basis-1/2 xl:text-3xl font-sans font-bold my-auto sm:text-2xl">{{tempCelcius}}<span>&#176;</span>C</p>
+    <div class="flex flex-col md:flex-row justify-around w-full h-max p-4">
+      <!-- Temperature Section -->
+      <div class="flex flex-col items-center md:basis-1/4 md:mb-0">
+        <p class="font-sans text-lg font-bold">Temperature</p>
+        <div class="flex items-center">
+          <img class="w-16 sm:w-12 xl:w-32" :src="require('../assets/images/icons/thermometer.svg')" alt="thermometer">
+          <p class="text-xl sm:text-2xl xl:text-3xl font-sans font-bold ml-2">{{ tempCelcius }}<span>&#176;</span>C</p>
         </div>
       </div>
 
-      <div class="basis-1/4">
-        <img
-          class="xl:w-screen xl:h-32 sm:w-32 sm:h-24"
-          :src="
-            require(`../assets/images/icons/${dataWeather.weather[0].main}.svg`)
-          "
-          alt="iconWeather"
-        />
-        <p class="font-sand font-bold text-3xl text-center">{{dataWeather.weather[0].main}}</p>
+      <!-- Weather Icon Section -->
+      <div class="flex flex-col items-center md:basis-1/4 md:mb-0">
+        <img class="w-24 h-24 sm:w-32 sm:h-24 xl:w-full xl:h-32"
+          :src="require(`../assets/images/icons/${dataWeather.weather[0].main}.svg`)" alt="iconWeather">
+        <p class="font-sans font-bold text-3xl mt-2">{{ dataWeather.weather[0].main }}</p>
       </div>
 
-      <div class="basis-1/4">
-        <div class="flex justify-around">
-          <img class="xl:w-24 sm:w-12" :src="require(`../assets/images/icons/wind.svg`)" alt="">
-          <p class="basis-1/2 xl:text-2xl font-sans font-bold my-auto sm:text-lg">{{dataWeather.wind.speed}}Km/h</p>
+      <!-- Wind Speed and Humidity Section -->
+      <div class="flex flex-col items-center md:basis-1/4">
+        <div class="flex flex-col items-center">
+          <p class="font-sans text-lg font-bold">Wind Speed</p>
+          <div class="flex items-center">
+            <img class="w-16 sm:w-12 xl:w-24" :src="require('../assets/images/icons/wind.svg')" alt="wind">
+            <p class="text-xl sm:text-lg xl:text-2xl font-sans font-bold ml-2">{{ dataWeather.wind.speed }} Km/h</p>
+          </div>
         </div>
-
-        <div class="flex justify-around">
-          <img class="xl:w-24 sm:w-12" :src="require(`../assets/images/icons/raindrop.svg`)" alt="">
-          <p class="basis-1/2 xl:text-2xl font-sans font-bold my-auto sm:text-lg">{{dataWeather.main.humidity}}%</p>  
+        <div class="flex flex-col items-center">
+          <p class="font-sans text-lg font-bold">Humidity</p>
+          <div class="flex items-center">
+            <img class="w-16 sm:w-12 xl:w-24" :src="require('../assets/images/icons/raindrop.svg')" alt="humidity">
+            <p class="text-xl sm:text-lg xl:text-2xl font-sans font-bold ml-2">{{ dataWeather.main.humidity }}%</p>
+          </div>
         </div>
       </div>
     </div>
@@ -46,23 +50,23 @@
 
 export default {
   name: "CardComp",
-  data(){
-    return{
-        city: this.$store.state.dataWeather.name,
+  data() {
+    return {
+      city: this.$store.state.dataWeather.name,
     }
   },
-  methods:{
+  methods: {
   },
   computed: {
     dataWeather() {
       return this.$store.getters.getData;
     },
-    tempCelcius(){
+    tempCelcius() {
       return Math.round(this.dataWeather.main.temp - 273.15)
     },
   },
-  created(){
-   
+  created() {
+
   },
 };
 </script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,49 +1,39 @@
 <template>
   <div class="container-xl">
-      <h1 
-        class="font-sans text-4xl font-bold tracking-wide text-blue-500 text-center"
-        :class="[bgColor]"
-      >
-        WEATHER APP
-      </h1>
-      
-      <section 
-        class="flex w-auto h-48 mx-auto "
-        :class="[bgColor]"
-      > 
-  
-          <div 
-            class="flex-initial w-3/4 h-40 bg-contain"
-            :class="[skyRender]"
-          >            
-          </div>
-          
-          <div class="flex-initial w-1/4 rounded-md">
-              <div class="p-1">
-                <p class="ml-5 text-blue-500 font-semibold underline decoration-lime-700">{{time}}</p>
-                <p class="ml-5 text-blue-500 font-semibold underline decoration-blue-900">{{date}}</p>
+    <h1 class="font-sans text-4xl font-bold tracking-wide text-blue-500 text-center" :class="[bgColor]">
+      Weather App
+    </h1>
 
-              </div>
+    <section class="flex w-auto h-48 mx-auto " :class="[bgColor]">
 
-              <img 
-                class="w-48" 
-                :src="require(`../assets/images/icons/${dayNight}.svg`)" 
-                alt="iconDayNight"
-              >
-          </div>
-
-      </section>
-
-      <FormComp :class="[bgColor]" />
-
-      <div class="container-xl" :class="[bgColor]">
-        <div 
-          class="w-full h-80 bg-repeat-x bg-auto bg-left bg-clip-content bg-[url('../assets/images/city.png')]"      
-        >
-          <CardComp v-if="dataWeather.cod === 200"  />
-          <p v-else></p>
-        </div>
+      <div class="flex-initial w-3/4 h-40 bg-contain" :class="[skyRender]">
       </div>
+
+      <div class="flex-initial w-1/4 rounded-md">
+        <div class="p-1">
+          <p class="ml-5 text-blue-500 font-semibold underline decoration-lime-700">{{ time }}</p>
+          <p class="ml-5 text-blue-500 font-semibold underline decoration-blue-900">{{ date }}</p>
+
+        </div>
+
+        <img class="w-48" :src="require(`../assets/images/icons/${dayNight}.svg`)" alt="iconDayNight">
+      </div>
+
+    </section>
+
+    <FormComp :class="[bgColor]" />
+
+    <div class="container-xl h-max" :class="[bgColor]">
+      <div 
+        v-if="dataWeather.cod === 200"
+        class="w-full h-max bg-repeat-x bg-auto bg-left bg-clip-content bg-[url('../assets/images/city.png')]">
+        <CardComp class="mx-auto" />
+      </div>
+      <div 
+        v-else
+        class="w-full h-80 bg-repeat-x bg-auto bg-left bg-clip-content bg-[url('../assets/images/city.png')]">
+      </div>
+    </div>
 
   </div>
 </template>
@@ -53,67 +43,69 @@ import CardComp from "../components/CardComp.vue";
 
 
 export default {
-    name: "HomePage",
-    components:{
-      FormComp,
-      CardComp,
-    },
-    data(){
-      return{
-        time:"",
-        date:"",
-        dayNight:"",
-        skyRender:"",
-        bgColor:"",
-        show:false
-      }
-    },
-    methods:{
-      getTime(){
-        let date = new Date();
-        let hours = date.getHours();
-        let minutes = date.getMinutes();
-        let ampm = hours >= 12 ? "PM" : "AM";
-        hours = hours % 12;
-        hours = hours ? hours : 12; // hour 0 should be 12
-        minutes = minutes < 10 ? '0'+minutes : minutes;
-        this.time = hours + ':' + minutes + ' ' + ampm;
-      },
-      getDate(){
-        let d = new Date();
-        let date = d.toDateString();
-        this.date = date;
-      },
-      getDayNight(){
-        let d = new Date();
-        let hours = d.getHours();
-        if (hours >= 6 && hours <= 15){
-          this.dayNight = 'Day';
-          this.skyRender = `bg-[url('../assets/images/cloud.png')]`;
-        } else if (hours >= 16 && hours <= 18){
-          this.dayNight = 'Sunset';
-          this.skyRender = `bg-[url('../assets/images/cloud.png')]`;
-        } else {
-          this.dayNight = 'Night';
-          this.skyRender = `bg-[url('../assets/images/stars.png')]`;
-          this.bgColor = 'bg-slate-800';
-        }
-      }
-    },
-    computed:{
-      dataWeather(){
-        return this.$store.getters.getData;
-      },
-    },
-    created(){
-      this.getTime();
-      this.getDate();
-      this.getDayNight();
+  name: "HomePage",
+  components: {
+    FormComp,
+    CardComp,
+  },
+  data() {
+    return {
+      time: "",
+      date: "",
+      dayNight: "",
+      skyRender: "",
+      bgColor: "",
+      show: false
     }
+  },
+  methods: {
+    getTime() {
+      let date = new Date();
+      let hours = date.getHours();
+      let minutes = date.getMinutes();
+      let ampm = hours >= 12 ? "PM" : "AM";
+      hours = hours % 12;
+      hours = hours ? hours : 12; // hour 0 should be 12
+      minutes = minutes < 10 ? '0' + minutes : minutes;
+      this.time = hours + ':' + minutes + ' ' + ampm;
+    },
+    getDate() {
+      let d = new Date();
+      let date = d.toDateString();
+      this.date = date;
+    },
+    getDayNight() {
+      let d = new Date();
+      let hours = d.getHours();
+      if (hours >= 6 && hours <= 15) {
+        this.dayNight = 'Day';
+        this.skyRender = `bg-[url('../assets/images/cloud.png')]`;
+      } else if (hours >= 16 && hours <= 18) {
+        this.dayNight = 'Sunset';
+        this.skyRender = `bg-[url('../assets/images/cloud.png')]`;
+      } else {
+        this.dayNight = 'Night';
+        this.skyRender = `bg-[url('../assets/images/stars.png')]`;
+        this.bgColor = 'bg-slate-800';
+      }
+    }
+  },
+  computed: {
+    dataWeather() {
+      return this.$store.getters.getData;
+    },
+  },
+  created() {
+    this.getTime();
+    this.getDate();
+    this.getDayNight();
+  },
+  mounted() {
+    setInterval(() => {
+      this.getTime();
+    }, 1000);
+  }
 }
 </script>
 
-<style lang="postcss">
-
-
-</style>
+<style lang="postcss"></style>
